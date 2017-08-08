cbfaf92470afc930cf6756f02765f1231f085abb
Adrian31 <adrian_carabetta@hotmail.com>
2017-08-08 13:29:08 +0800

    Added the cat picture and got a counter working. Bugs mentioned in comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-$( "#target" ).click(function() {
-  alert( "Handler for .click() called." );
+var $body = $('body');
+var clicks = 0;
+//$body.append('<img class="bgimg" src="'cat.jpg'">');
+$("#cat").prepend('<img id="cat" src="images/cat.jpg" />');
+
+$( "#cat" ).click(function() {
+  //alert( "Handler for .click() called." );
+  if(clicks >= 20){
+    clicks = clicks +2;
+  }else{
+    clicks++;
+  }
+
+  $('.counter').html(clicks);
+
 });
