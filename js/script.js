0cda31f5d09e1a1b6156ca8a247f139892d35571
Adrian31 <adrian_carabetta@hotmail.com>
2017-08-09 14:21:02 +0800

    Added second cat and names

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
 var $body = $('body');
 var clicks = 0;
-//$body.append('<img class="bgimg" src="'cat.jpg'">');
-$("#cat").prepend('<img id="cat" src="images/cat.jpg" />');
+var img = document.createElement("img");
 
-$( "#cat" ).click(function() {
-  //alert( "Handler for .click() called." );
-  if(clicks >= 20){
-    clicks = clicks +2;
-  }else{
-    clicks++;
+var catClick = function(){
+
+  //#cat needs to become the image tag
+  $( "#cat" ).click(function() {
+    if(clicks >= 20){
+      clicks = clicks +2;
+    }else{
+      clicks++;
+    }
+    //Displays the amount of times the cat was clicked.
+    $('.counter').html(clicks);
+  });
+}
+
+var catAdd = function(){
+
+  for (var i = 1; i <= 2; i++) {
+      var img = document.createElement("img");
+      img.src = "images/cat" + i + ".jpg";
+
+      $("#cat").prepend(img);
+      $("#cat").prepend("<h1 class='header'>Cat</h1>");
   }
+};
 
-  $('.counter').html(clicks);
+catAdd();
+catClick();
 
-});
+
 //Known Bugs
 //Clicking to the side of the cat also increments the counter.
+//Cats names not being put above the images.
